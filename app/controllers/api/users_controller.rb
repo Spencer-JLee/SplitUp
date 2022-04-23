7091806958027a9d2acefc6f9c812878b74c3924
Spencer-JLee <spencer.lee.careers@gmail.com>
2022-04-22 21:41:54 -0400

    edited webpack.confiig

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
 
     def create
         @user = User.new(user_params)
@@ -13,6 +14,6 @@ class Api::UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :email, :password)
     end
 end
